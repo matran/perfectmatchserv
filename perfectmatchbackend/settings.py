714acb9eb8ab25adc106e19271b8456bbe9d2d45
henry cheruiyot <matranic@gmail.com>
2023-02-07 00:32:05 +0300

    removephoto

--- perfectmatchbackend/settings.py
+++ perfectmatchbackend/settings.py
@@ -84,15 +84,15 @@ WSGI_APPLICATION = 'perfectmatchbackend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
-     #  'default': {
-     #   'ENGINE': 'django.db.backends.postgresql',
-      #  'NAME': 'perfectmatch',
-      #  'USER': 'postgres',
-       # 'PASSWORD': 'henry5765',
-       # 'HOST': '127.0.0.1',
-       # 'PORT': '5432',
-    #}
+    # 'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+      'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'perfectmatch',
+        'USER': 'postgres',
+        'PASSWORD': 'henry5765',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
 }
 
 
